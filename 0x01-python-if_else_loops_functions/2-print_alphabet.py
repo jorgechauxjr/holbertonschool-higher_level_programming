c7cd873950c34c0867140b2c5eca7a161c9c143c
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 19:29:41 +0000

    2 print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-        print("{0:c}".format(i), end="")
+    print("{:c}" .format(i), end="")
 # "end" on print func avoid \n and allow me to append in the same line
 # 97 and 123 are the ascii code for the letters
