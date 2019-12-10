10c17653d231f4fb35673809f5806d3ed3093dcd
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 22:42:12 +0000

    0x01-python-if_else_loops_functions 12-fizzbuzz.py space added to Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for n in range(1, 101):
         if n == 100:
-            print("Buzz", end="")
+            print("Buzz ", end="")
         elif n % 3 != 0 and n % 5 != 0:
             print("{0:d} ".format(n), end="")
         elif n % 3 == 0 and n % 5 == 0:
