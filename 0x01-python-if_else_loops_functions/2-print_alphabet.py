59307bf9d1672a84a2f0f9a355ef25d57a62c979
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 19:26:10 +0000

    0x01-python-if_else_loops_functions 1-last_digit.py ascii numb added

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-        print(chr(i), end="")
-# ord() function receives a character and return the ascii code (integer)
-# in this case receives 'a '97 and'z'122.
-# The "for" will not print the last value (z) for that reason I need to add 1.
+for i in range(97, 123):
+        print("{0:c}".format(i), end="")
 # "end" on print func avoid \n and allow me to append in the same line
-# chr() receives ascii code (integer) and return the character.
+# 97 and 123 are the ascii code for the letters
