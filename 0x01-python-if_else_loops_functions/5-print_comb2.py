06d5d84717d953f05522baa91d8f31243729c4e7
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 19:50:35 +0000

    0x01-python-if_else_loops_functions 5-print_comb2.py final comments

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,3 +4,6 @@ for num in range(0, 100):
         print("{0:02d}, ".format(num), end="")
     else:
         print("{0:02d}".format(num))
+# numbers can be constrained to a specific width
+# {0:02d} 2 is the width of the number. "d" means decimal
+# and 0 is the number that will fill the empty space.
