e824b1051071d0f3375a2d25ac5fc81032407b09
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 18:35:49 +0000

    0x01-python-if_else_loops_functions 1-last_digit.py comments at the end

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-# The "end" key of print function will set the string that needs
-#  to be appended when printing is done.
-# ends=""allow me to append and print in the conitionals, instead of new line.
 if number < 0:
     lastDigit = (number * -1) % 10
 else:
@@ -18,3 +14,6 @@ elif lastDigit == 0:
     print("and is 0".format(number, lastDigit))
 else:
     print("and is less than 6 and not 0".format(number, lastDigit))
+# The "end" key of print function will set the string that needs
+#  to be appended when printing is done.
+# ends=""allow me to append and print in the conitionals, instead of new line.
