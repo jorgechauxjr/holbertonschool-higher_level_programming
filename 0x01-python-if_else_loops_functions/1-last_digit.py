e605143bd3ae76f4029516f0edab467f6feb1f17
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 18:42:35 +0000

    0x01-python-if_else_loops_functions 1-last_digit.py delete .format in conditionals

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,18 +2,19 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    lastDigit = (number) % 10 * -1
+    lastDigit = (number * -1) % 10
 else:
     lastDigit = number % 10
+
+# The "end" key of print function will set the string that needs
+#  to be appended when printing is done.
+# ends=""allow me to append and print in the conitionals, instead of new line.
 
 print("Last digit of {0:d} is {1:d} ".format(number, lastDigit), end="")
 
 if lastDigit > 5:
-    print("and is greater than 5".format(lastDigit))
+    print("and is greater than 5")
 elif lastDigit == 0:
-    print("and is 0".format(number, lastDigit))
+    print("and is 0")
 else:
-    print("and is less than 6 and not 0".format(number, lastDigit))
-# The "end" key of print function will set the string that needs
-#  to be appended when printing is done.
-# ends=""allow me to append and print in the conitionals, instead of new line.
+    print("and is less than 6 and not 0")
