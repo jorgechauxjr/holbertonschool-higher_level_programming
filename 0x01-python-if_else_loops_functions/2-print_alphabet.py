a3086f7aaf8c0b899012ef9302afbf115e3468b4
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 14:48:25 +0000

    0x01-python-if_else_loops_functions 2-print_alphabet.py comments

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -4,3 +4,4 @@ for i in range(ord('a'), ord('z')+1):
 # ord() function is to take the ascii value of the character
 # in this case'a'takes 97 and'z' 122.
 # The "for" will not print the last value (z) foor that reason I need to add 1
+# "end" on print func avoid \n and allow me to append in the same line
