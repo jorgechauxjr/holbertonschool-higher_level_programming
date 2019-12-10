845c5373f4e51ca09aa5504127b6e6a25c936296
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 14:56:04 +0000

    0x01-python-if_else_loops_functions 2-print_alphabet.py comments fixed

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
         print(chr(i), end="")
-# ord() function is to take the ascii value of the character
-# in this case'a'takes 97 and'z' 122.
-# The "for" will not print the last value (z) foor that reason I need to add 1
+# ord() function receives a character and return the ascii code (integer)
+# in this case receives 'a '97 and'z'122.
+# The "for" will not print the last value (z) for that reason I need to add 1.
 # "end" on print func avoid \n and allow me to append in the same line
-# chr() receivves ascii code (integer) and return the character.
+# chr() receives ascii code (integer) and return the character.
