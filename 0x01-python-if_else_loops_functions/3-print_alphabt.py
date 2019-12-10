edec2e605f10b3b365da854aef48873031503c1c
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 15:06:43 +0000

    0x01-python-if_else_loops_functions 3-print_alphabt.py comments

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,7 +4,9 @@ for i in range(ord('a'), ord('z') + 1):
                 continue
         print(chr(i))
 # ord() function receives a character and return the ascii code (integer)
-# The "continue" statement rejects all the remaining statements
-# in the current iteration of the loop and moves the control
-# back to the top of the loop.
+'''
+The "continue" statement rejects all the remaining statements
+in the current iteration of the loop and moves the control
+back to the top of the loop.
+'''
 # For that reason the 'q' and 'e' are not printed.
