d71bf6bf56a5663ff7afac6950b40d7aecb5f2ba
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-10 18:30:28 +0000

    0x01-python-if_else_loops_functions 1-last_digit.py add validations at the beginning

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,16 +5,16 @@ number = random.randint(-10000, 10000)
 # The "end" key of print function will set the string that needs
 #  to be appended when printing is done.
 # ends=""allow me to append and print in the conitionals, instead of new line.
-print("Last digit of {0:d} is ".format(number), end="")
-
 if number < 0:
     lastDigit = (number * -1) % 10
 else:
     lastDigit = number % 10
 
+print("Last digit of {0:d} is {1:d}".format(number, lastDigit), end="")
+
 if lastDigit > 5:
-    print("{0:d} and is greater than 5".format(lastDigit))
+    print(" and is greater than 5".format(lastDigit))
 elif lastDigit == 0:
-    print("{1:d} and is 0".format(number, lastDigit))
+    print(" and is 0".format(number, lastDigit))
 else:
-    print("{1:d} and is less than 6 and not 0".format(number, lastDigit))
+    print(" and is less than 6 and not 0".format(number, lastDigit))
