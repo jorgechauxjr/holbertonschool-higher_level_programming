4ab47d4b164af9aac50c2513912780b5d0aa42cc
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-12 21:09:52 +0000

    0x02-python-import_modules 102-magic_calculation.py function defined

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -8,4 +9,3 @@ from magic_calculation_102 import add, sub
 
     else:
         return(sub(a, b))
-    return(0)
