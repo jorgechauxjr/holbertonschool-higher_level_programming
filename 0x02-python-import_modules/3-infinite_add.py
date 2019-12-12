2c87442474d529ad42907f8fea093a12d3b2258e
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-12 18:28:16 +0000

    0x02-python-import_modules 3-infinite_add.py comments removed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ from sys import argv
 if __name__ == '__main__':
     result = 0
     for i in argv[1:]:
-        result += int(i) #i receive value of argv that are str
-    print(result) #for that reason I cast to int
+        result += int(i)
+    print(result)
