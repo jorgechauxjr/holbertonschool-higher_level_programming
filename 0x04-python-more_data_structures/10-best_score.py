c006c0bb1c25b678d80dc551048c4a6d60b6d806
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-19 22:01:01 +0000

    0x04-python-more_data_structures 10-best_score.py assign key value final

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,5 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        max_key_value = max(a_dictionary)
-        key = max_key_value
-        return (key)
+        max_key_value = max(a_dictionary, key=lambda x: a_dictionary[x])
+        return max_key_value
