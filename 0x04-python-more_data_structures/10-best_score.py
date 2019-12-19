ab2e64727a224b4a01782051062a8ab525d3a611
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-19 22:05:54 +0000

    0x04-python-more_data_structures 10-best_score.py validation if dictionary == {} added

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionaty == {}:
         return None
     else:
         max_key_value = max(a_dictionary, key=lambda x: a_dictionary[x])
