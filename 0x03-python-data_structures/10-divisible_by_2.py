eb7fe5d372a2562f332d3bcdffd81338303f17db
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-30 20:34:37 +0000

    0x03-python-data_structures 10-divisible_by_2.py Optimized Code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
-    len_list = len(my_list)
-    for i in range(len_list):
-        if (my_list[i] % 2 == 0):
-            new_list[i] = True
-        else:
-            new_list[i] = False
+    new_list = [True if i %2 == 0 else False for i in my_list]
     return new_list
+# ternary operator and list comprehension in python
