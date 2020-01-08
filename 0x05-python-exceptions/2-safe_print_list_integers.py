50bbc59cb92e986c3d38937a57c1c91d041af4c6
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-08 18:30:49 +0000

    0x05-python-exceptions 2-safe_print_list_integers.py finally statement removed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
-    try:
-        for elements in my_list[0:x]:
+    for elements in my_list[0:x]:
+        try:
             print("{:d}".format(elements), end="")
             counter += 1
-    except (IndexError, ValueError, TypeError):
-        print("")
-    finally:
-        print()
-        return counter
+        except (IndexError, ValueError, TypeError):
+            pass
+    print()
+    return counter
