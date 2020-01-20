94e9dfdfb193eedbdec57e03e27836e398767213
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-20 23:03:29 +0000

    0x0A-python-inheritance 9-rectangle.py added code from task 8 to inherit

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Public instance method that validates value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)"""
+    """Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+    (task based on 8-rectangle.py)"""
 
     def __init__(self, width, height):
         """Constructor method for Rectangle
