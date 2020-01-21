28019efaf582a68b44b2aaf1097537e090071517
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-21 22:06:20 +0000

    0x0B-python-input_output 10-class_to_json.py

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def class_to_json(obj):
+    """Function that returns the dictionary description with simple
+    data structure for JSON serialization of an object"""
+
+    return obj.__dict__
