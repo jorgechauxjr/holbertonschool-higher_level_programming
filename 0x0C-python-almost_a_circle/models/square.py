4d5ddba8beef51108b41fc6ecf27cc44061b3cc0
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-25 00:53:49 +0000

    0x0C-python-almost_a_circle models/rectangle.py 12.Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """String representation of class."""
+        """String representation of class"""
 
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
@@ -27,3 +27,21 @@ class Square(Rectangle):
 
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update method for Square"""
+
+        if args:
+            for i, element in enumerate(args):
+                if i == 0:
+                    self.id = element
+                elif i == 1:
+                    self.size = element
+                elif i == 2:
+                    self.x = element
+                elif i == 3:
+                    self.y = element
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
