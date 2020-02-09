61a9918af490d6edce9dacb9c6240b65a734a289
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-09 00:56:17 +0000

    Delete baseGeometry and Add __import__

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
+"""Rectangle module"""
 
 
-class BaseGeometry:
-    """Class BaseGeometry"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Public instance method that validates value"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
