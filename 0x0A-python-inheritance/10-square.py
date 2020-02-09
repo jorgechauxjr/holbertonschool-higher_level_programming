9b913922d396874d646b24db37c1e7b9496df315
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-09 01:09:03 +0000

    Delete begining and Add __import__ to 10-square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,43 +1,10 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """Class BaseGeometry"""
 
-    def area(self):
-        raise Exception("area() is not implemented")
+"""
+module for Square.
+"""
 
-    def integer_validator(self, name, value):
-        """ Public instance method that validates value"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
-    (task based on 8-rectangle.py)"""
-
-    def __init__(self, width, height):
-        """Constructor method for Rectangle
-        Instantiation with width and height"""
-
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Return area of rectangle"""
-
-        area = self.__width * self.__height
-        return area
-
-    def __str__(self):
-        """Returns string representation of a Rectangle"""
-
-        string = "[Rectangle] {}/{}".format(self.__width, self.__height)
-        return string
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
