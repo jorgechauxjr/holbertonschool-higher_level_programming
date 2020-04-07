71265d9797258baa32ea8eac9d31bb8de211088f
jorgechauxjr <chauxjorge@hotmail.com>
2020-04-07 02:03:26 +0000

    Add validation if list have only 1 or 2 elements 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,11 @@ def find_peak(list_of_integers):
     peak, number, prev = 0, 0, 0
     first = True
 
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        return max(list_of_integers)
+
     if list_of_integers:
         for number in list_of_integers:
             if first is True:
