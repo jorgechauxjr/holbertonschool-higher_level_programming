034a02463538751a0a6edb63bfbfe9da633510e8
jorgechauxjr <chauxjorge@hotmail.com>
2020-05-05 16:24:51 +0000

    Add 1 second bigest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg = process.argv;
+const size = arg.length;
+// process.argv occupies the first two positions of the array
+// The first element is the process execution path
+// the second element is the path for the js file.
+if (size <= 3) {
+  console.log(0);
+} else {
+  arg.sort();
+  const secondBiggest = arg[size - 2];
+  console.log(secondBiggest);
+}
